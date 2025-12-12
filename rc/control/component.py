edb7fb353435ee04098bf0bf3728d0b467e7b60a
RYAN RIVERA <rrivera@FNAL.GOV>
2025-12-12 12:05:01 -0600

    Whitespace cleanup

--- rc/control/component.py
+++ rc/control/component.py
@@ -109,9 +109,9 @@ class Component(ContextObject):
     # def state(self, name):
     #     print(f"getting state for component {name}, my name is {self.name}")
     #     if name != self.name:
-    #         return "unknown"      
+    #         return "unknown"
 
-    #     if hasattr(obj, f"_{obj.__class__.__name__}__state"):  
+    #     if hasattr(obj, f"_{obj.__class__.__name__}__state"):
     #         print(f"getting state for component {name}, my name is {self.name}, my state is {self.__state}")
     #     else:
     #         print(f"Unknown state for component {name}, my name is {self.name}")
